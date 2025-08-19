6631a371d264081e419337dfdf48a7302ba90178
denzhe dzebu <dedzebujhb024@student.wethinkcode.co.za>
2025-08-19 11:59:31 +0200

    test

--- src/server/app.js
+++ src/server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+
+// Import and use our socket.js module
+const io = require('./socket')(server);
 
 // Middleware
 app.use(express.json());
@@ -19,19 +20,6 @@ mongoose.connect('mongodb://localhost:27017/whiteboard', {
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
-
-// Socket.io setup
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('drawing', (data) => {
-        socket.broadcast.emit('drawing', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
-});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
